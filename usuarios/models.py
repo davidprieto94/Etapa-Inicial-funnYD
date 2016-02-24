1ffe50d3ca4c8445590cfebdca3cdc4a2506a0ad
Juan Sandoval <juancsandoval86@gmail.com>
2016-02-24 17:26:24 -0500

    configuracion del proyecto

---
+++ usuarios/models.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+GENEROS = (
+		('M', 'Masculino'),
+		('F', 'Femenino'),
+	)
+
+class usuariosModel(models.Model):
+	user = models.OneToOneField(User)
+	slug = models.SlugField()
+	documento = models.PositiveIntegerField(null=True, blank=True)
+	genero = models.CharField(max_length=2, choices=GENEROS, null=False, blank=False)
+	fnacimiento = models.DateField()
