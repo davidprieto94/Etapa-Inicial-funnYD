98751fc73ee27fd36403fc109e9ed467ee2a5baa
David Prieto <davidprieto.94@hotmail.com>
2016-06-16 21:27:09 -0500

    Creación vista descripción Rutina Diaria

--- actividades/urls.py
+++ actividades/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from django.contrib import admin
 import actividades.views
 
+
 urlpatterns = [
     url(r'^actividades/$', actividades.views.listActividades),
+    url(r'^descripcionRD/$', actividades.views.descripcionRD),
 ]
--- actividades/views.py
+++ actividades/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def listActividades(request):	
 	return render(request, "list_actividades.html")
+
+def descripcionRD(request):	
+	return render(request, "descr_rutinadiaria.html")
